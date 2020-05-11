2de9586d7ef96af16313becd21b92c7df8ab67c6
jordanforbes <jordforb@gmail.com>
2020-05-11 09:27:14 -0400

    need to make the numbers re randomize

--- server.py
+++ server.py
@@ -10,18 +10,25 @@ jobs = {
         'house': random.randint(2,5),
         'casino': random.randint(-50,50)
     }
+message=[]
 
 @app.route('/', methods=['GET','POST'])
 def home():
     if 'gold' not in session:
         session['gold'] = 0
+        
     
     print(jobs)
-    return render_template('index.html', jobs = jobs)
+    return render_template('index.html', message=message, jobs = jobs, times = len(message))
 
 @app.route('/process_money', methods=['POST'])
 def process():
     session['gold']+= jobs[request.form['job']]
+    if(jobs[request.form['job']]>0):
+        message.append('<p style="color:green;">'+request.form['job']+' gained'+ '</p>')
+    else:
+        message.append('<p style="color:red;">'+request.form['job']+ 'lost'+ '</p>')
+
     return redirect('/')
 
 if __name__ =="__main__":
