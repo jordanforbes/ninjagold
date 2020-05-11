52ef79ce5f8ff8275760aed3acdf7a11aef95ad8
jordanforbes <jordforb@gmail.com>
2020-05-10 23:32:19 -0400

    worked on forms

--- server.py
+++ server.py
@@ -1,3 +1,26 @@
-from flask import Flask, render_template, redirect, session
-app=(__name__)
+from flask import Flask, render_template, redirect, session, request
+import random
 
+app= Flask(__name__)
+app.secret_key="it's a secret"
+
+@app.route('/')
+def home():
+    if 'gold' not in session:
+        session['gold'] = 0
+    
+    jobs = {
+        'farm': random.randint(10,20),
+        'cave': random.randint(5,10),
+        'house': random.randint(2,5),
+        'casino': random.randint(-50,50)
+    }
+    return render_template('index.html', jobs = jobs)
+
+@app.route('/process_money')
+def process():
+    
+    return redirect('/')
+
+if __name__ =="__main__":
+    app.run(debug=True)
