7fb46f9d57f26b0ac380b5a7ce953b11152cc473
jordanforbes <jordforb@gmail.com>
2020-05-11 09:06:43 -0400

    created way to add gold

--- server.py
+++ server.py
@@ -4,22 +4,24 @@ import random
 app= Flask(__name__)
 app.secret_key="it's a secret"
 
-@app.route('/')
-def home():
-    if 'gold' not in session:
-        session['gold'] = 0
-    
-    jobs = {
+jobs = {
         'farm': random.randint(10,20),
         'cave': random.randint(5,10),
         'house': random.randint(2,5),
         'casino': random.randint(-50,50)
     }
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if 'gold' not in session:
+        session['gold'] = 0
+    
+    print(jobs)
     return render_template('index.html', jobs = jobs)
 
-@app.route('/process_money')
+@app.route('/process_money', methods=['POST'])
 def process():
-    
+    session['gold']+= jobs[request.form['job']]
     return redirect('/')
 
 if __name__ =="__main__":
